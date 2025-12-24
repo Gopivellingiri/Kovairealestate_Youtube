cf706857dec12f350f2dea0bc63d214a2039627d
Gopivellingiri <designergopi.cbe@gmail.com>
2025-12-24 06:12:07 +0530

    vite and tailwind installation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./shared/Header";
+import { HomePage } from "./routes/PublicRoutes";
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
