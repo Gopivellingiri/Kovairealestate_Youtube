2de444fb463e8669c38bff48a07ca9bed95341f4
Gopivellingiri <designergopi.cbe@gmail.com>
2026-01-06 07:50:29 +0530

    backend creation

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
-// backend entry point
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+export default app;
