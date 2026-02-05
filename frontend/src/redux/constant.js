90c8889f07ce0105fa5eb910e8822fea9efacb2d
Gopivellingiri <designergopi.cbe@gmail.com>
2026-02-05 10:34:01 +0530

    adding production and development .env

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,4 +1,4 @@
-const localHost = "http://localhost:7000";
+const localHost = import.meta.env.VITE_API_BASE_URL;
 
 export const BASE_URL = `${localHost}/api/v1`;
 export const USER_URL = `${BASE_URL}/users`;
