501e97deda6a214c92757f914a4b18da981fbe93
Gopivellingiri <designergopi.cbe@gmail.com>
2026-02-05 11:01:59 +0530

    modifying cors

--- backend/app.js
+++ backend/app.js
@@ -12,8 +12,28 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+  "https://realestatekovaiweb.netlify.app",
+  "http://localhost:5173",
+];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow server-to-server, Postman, curl
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 // main route definition
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/properties", propertyRoutes);
